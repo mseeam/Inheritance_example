46f507f1f152f7e5d956c47d3bcea6b3bee57fc3
mseeam <mseeam99@gmail.com>
2023-03-10 09:57:48 -0600

    initial commit

---
+++ SuperClass.java
@@ -0,0 +1,11 @@
+public class SuperClass{
+    /** Constructor_1  */
+    public SuperClass(){
+        System.out.println("This is superclass" + " Constructor");
+    }
+
+    /** Constructor_2  */
+    public SuperClass(int arg){
+        System.out.println("This is superclass" + " Constructor with argument " + arg);
+    }
+}cd --
